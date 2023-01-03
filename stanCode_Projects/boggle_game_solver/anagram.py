b0706896a4852848346c5afc73ab8e3a8451f8a3
Tingyu98 <115769484+Tingyu98@users.noreply.github.com>
2023-01-03 16:06:59 +0800

    Add files via upload

--- stanCode_Projects/boggle_game_solver/anagram.py
+++ stanCode_Projects/boggle_game_solver/anagram.py
@@ -1,7 +1,6 @@
 """
 File: anagram.py
-Name: GimmyLee 李庭侃
-Time: 111/05/27 (五) ~ 111/06/03 (五)
+Name:Ting-Yu
 ----------------------------------
 This program recursively finds all the anagram(s)
 for the word input by user and terminates when the
@@ -26,160 +25,84 @@ EXIT = '-1'                   # Controls when to stop the loop
 
 def main():
     """
-    1. Print the Welcome.
-    2. Let user enter word, and remove the front and back space, to achieve case-insensitive.
-
-    3. Judgment:
-        3-1. If the input is equal to EXIT, end the program.
-        3-2. If the input is word, continue to find the anagram.
+    Input a word and find its anagrams
     """
-    print('Welcome to stanCode "Anagram Generator" (or', EXIT, 'to quit)')      # 1. Print the Welcome.
-
+    print("Welcome to stanCode \"Anagram Generator\" (or -1 to quit)")
     while True:
-        user_input_s = input('Type a word: ').strip().lower()                   # 2. User entered words.
-        if user_input_s == EXIT:                                                # 3-1. End the program
+        s = input("Find anagrams for: ")
+        if s == EXIT:
             break
-        else:
-            find_anagrams(user_input_s)                                         # 3-2. Continue to find the anagram.
-
-
-def read_dictionary(target_s):
-    """
-    :param target_s: string, user entered words.
-    :return alphabet: list[string], find the existence of the word from the dictionary.
-    -----------------------------------------------------------------------------
-    This function mainly reads the file and reads English words from dictionary.txt.
-
-    Basic Framework
-        1. Read the file.
-        2. Read the contents of the text file line by line.
-        3. Remove the newline (\n) from the end of the sentence.
-        4. Append the word to the list.
-    Optimization
-        Ⅰ. Judgment:
-            If the word in the dictionary is of the same length as the word entered by the user,
-            and by judging the letter of the word.
-    """
-    alphabet = []
-
-    with open(FILE, 'r') as f:              # 1. Read the file.
-        for word in f:                      # 2. Read the contents.
-            word = word.strip()             # 3. Remove the newline (\n).
-            if len(word) == len(target_s) and letter_check(word, target_s):     # Ⅰ. word length should be the same
-                alphabet.append(word)  # 4. Append the word to the list.
-
-    return alphabet
-
-
-def letter_check(word, target_s):
-    """
-    :param word: string, find the word of the same length in the dictionary
-    :param target_s: string, user entered words.
-    :return: bool, Is the anagram of the word.
-    -----------------------------------------------------------------------------
-    Used to determine the number of letters in a word,
-    if the number of letters in the dictionary is less than the one entered by the user,
-    it means that the word will not be reorganized.
-
-    e.g.
-        word     == 'aardvark'
-        target_s == 'contains'
-        letter   == 'c'
-
-    At this time, the target_s word contains the letter c, so the number is 1.
-    the word doesn't have the letter c in it, so the number is 0,
-    since there is no c in word, it cannot be a reorganization of target_s.
-    """
-    for letter in target_s:
-        if word.count(letter) < target_s.count(letter):
-            return False
-    return True
-
-
-def timer(func):
-    """
-    :param func: function, executed function.
-    -----------------------------------------------------------------------------
-    Written in decorator format,
-    mainly used to test the execution time of find_anagrams().
-    """
-    def wrapper(s):
         start = time.time()
-        func(s)
+        find_anagrams(s)
         end = time.time()
         print('----------------------------------')
-        print(f'The speed of your anagram algorithm: {end - start} seconds.')
-    return wrapper
+        print(f'The speed of your anagram algorithm: {end-start} seconds.')
 
 
-@timer
+def read_dictionary(s):
+    """
+    :param s: string, the word input by user
+    :return lst: lst, a data container that stores the dictionary which will be looked up
+    """
+    lst = []
+    with open(FILE, "r") as f:
+        for word in f:
+            word = word.strip()
+            if len(word) == len(s):
+                lst.append(word)
+        return lst
+
+
 def find_anagrams(s):
     """
-    :param s: string, user entered words.
-    ----------------------------------------------
-    Check the words entered by the user to find out if there are any reorganized words.
-
-    1. Read the dictionary: search the words entered by the user.
-    2. Create list: after find_anagrams_helper process, the answer will be attached to this list, the purpose of creating here is to print the final result (number + regrouped words). 3.
-    3. Call helper: the function that actually finds the function to reorganize the words.
+    :param s: string, the word input by user
     """
-    dictionary_list = read_dictionary(s)                    # 1. Read the dictionary
-
-    print('Searching...')
-    ans_lst = []                                            # 2. Create list
-    find_anagrams_helper(s, '', ans_lst, dictionary_list)   # 3. Call helper
-
-    print(f'{len(ans_lst)} anagram: {ans_lst}')
+    dic_lst = read_dictionary(s)
+    lst = []
+    print("Searching...")
+    find_anagrams_helper(s, "", len(s), dic_lst, lst, [],)
+    print(f"{len(lst)} anagrams: {lst}")
 
 
-def find_anagrams_helper(s, current_s, ans_lst, dictionary_list):
+def find_anagrams_helper(s, current_s, ans_len, dict_lst, lst, index):
     """
-    :param s: string, user entered words.
+    :param s: string, the word input by user
     :param current_s: string, used for concatenate prefix.
-    :param ans_lst: string, record the anagram that match the answer.
-    :param dictionary_list: list[str], list of words stored in the dictionary for optimizing recursion.
-    -----------------------------------------------------------------------------
-    The main purpose is to perform anagram word reorganization.
-
-    Basic Framework
-        1. Recursive Case
-            1-1. choose     : Insert a letter at the end
-            1-2. explore    : recursion.
-            1-3. un-choose  : Remove a letter from the end.
-        2. Base Case
-            2-1. When the word "s" is given to all "current_s".
-            2-2. and current_s is not in the answer before it is printed (to avoid duplicate answers).
-    Optimization
-        Ⅰ. If the first word is in the dictionary, then explore will continue.
+    :param ans_len: int, the length of the word input by user
+    :param dict_lst: list[str], list of words stored in the dictionary for optimizing recursion.
+    :param lst: list[str], record the anagram that match the answer
+    :param index: list[str], record the index that its corresponding letters already used in current_s
     """
-    if len(s) == 0:                                 # 2. Base Case
-        if current_s not in ans_lst:                    # 2-2
-            ans_lst.append(current_s)
-            print('Found:', current_s)
-            print('Searching...')
+    if len(current_s) == ans_len:  # base cas
+        if current_s in dict_lst:
+            if current_s not in lst:
+                print("Found: " + current_s)
+                print("Searching...")
+                lst.append(current_s)
+    else:
+        for i in range(ans_len):
+            if len(current_s) == ans_len:
+                break
+            if i not in index:  # back-tracking
+                # Choose
+                if i not in index:
+                    index.append(i)
+                    current_s += s[i]
+                # Explore
+                    if has_prefix(current_s, dict_lst):  # early-stopping
+                        find_anagrams_helper(s, current_s, ans_len, dict_lst, lst, index)
+                # Un-choose
+                    current_s = current_s[0:len(current_s)-1]
+                    index.pop()
 
-    else:                                           # 1. Recursive Case
-        for i in range(len(s)):
-            # 1-1. Choose
-            current_s += s[i]
-            unused_s = s[0:i]+s[i+1:]
 
-            # 1-2. Explore
-            if has_prefix(current_s, dictionary_list):          # Ⅰ. The first word is in the dictionary.
-                find_anagrams_helper(unused_s, current_s, ans_lst, dictionary_list)
-
-            # 1-3. Un-choose
-            current_s = current_s[:-1]
-
-
-def has_prefix(sub_s, dictionary_list):
+def has_prefix(sub_s, dictionary):
     """
-    The recursive case is performed only if the user enters a word whose initials are dictionary words.
-
-    e.g.
-        To find the word with c prefix, cat will return True and act will return False.
+    :param sub_s: string, the prefix to search for
+    :param dictionary: list[str], list of words stored in the dictionary for optimizing recursion.
+    :return: boolean
     """
-    for word in dictionary_list:
+    for word in dictionary:
         if word.startswith(sub_s):
             return True
     return False
--- stanCode_Projects/boggle_game_solver/sierpinski.py
+++ stanCode_Projects/boggle_game_solver/sierpinski.py
@@ -1,7 +1,6 @@
 """
 File: sierpinski.py
-Name: GimmyLee 李庭侃
-Time: 111/05/24 (二)
+Name: TingYu
 ---------------------------
 This file recursively prints the Sierpinski triangle on GWindow.
 The Sierpinski triangle is a fractal described in 1915 by Waclaw Sierpinski.
@@ -13,13 +12,12 @@ from campy.graphics.gobjects import GLine
 from campy.gui.events.timer import pause
 
 # Constants
-ORDER = -6				# Controls the order of Sierpinski Triangle
-LENGTH = 600			# The length of order 1 Sierpinski Triangle
-UPPER_LEFT_X = 150		# The upper left x coordinate of order 1 Sierpinski Triangle
-UPPER_LEFT_Y = 100		# The upper left y coordinate of order 1 Sierpinski Triangle
-WINDOW_WIDTH = 950		# The width of the GWindow
-WINDOW_HEIGHT = 700		# The height of the GWindow
-DELAY = 10				# The pause time
+ORDER = 6                  # Controls the order of Sierpinski Triangle
+LENGTH = 600               # The length of order 1 Sierpinski Triangle
+UPPER_LEFT_X = 150		   # The upper left x coordinate of order 1 Sierpinski Triangle
+UPPER_LEFT_Y = 100         # The upper left y coordinate of order 1 Sierpinski Triangle
+WINDOW_WIDTH = 950         # The width of the GWindow
+WINDOW_HEIGHT = 700        # The height of the GWindow
 
 # Global Variable
 window = GWindow(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)  # The canvas to draw Sierpinski Triangle
@@ -27,54 +25,31 @@ window = GWindow(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)  # The canvas to draw
 
 def main():
 	"""
-	TODO:
+	Use recursion to make a figure with Sierpinski Triangles
 	"""
 	sierpinski_triangle(ORDER, LENGTH, UPPER_LEFT_X, UPPER_LEFT_Y)
 
 
 def sierpinski_triangle(order, length, upper_left_x, upper_left_y):
 	"""
-	:param order: triangle layers.
-	:param length: triangle length.
-	:param upper_left_x: top left x-axis coordinate.
-	:param upper_left_y: top left y-axis coordinate.
-	:return: no return value.
-	-----------------------------------------------------------------
-
-	1. Drawing: draw triangle on the window.
-	2. Judgment: set the conditions of the Base Case and Recursive Case.
-		2-1. Base Case: when the order level is reduced to 0.
-		2-2. Recursive Case: when the order level is greater than 0,
-		2-3. Fool Proof design: this design simply prevents users from intentionally entering NEGATIVE order value.
-	3. Recursion: tends the Recursive Case condition to the Base Case state.
+	:param order: The order of Sierpinski Triangle
+	:param length: The length of Sierpinski Triangle
+	:param upper_left_x: The upper left x coordinate of Sierpinski Triangle
+	:param upper_left_y: The upper left y coordinate of Sierpinski Triangle
+	:return: A figure with Sierpinski Triangle
 	"""
-	# 2. Judgment
-	if order == 0:		# 2-1. Base Case
-		return
-
-	elif order < 0:		# 2-3 Fool Proof design
-		order *= -1
-		return sierpinski_triangle(order, length, upper_left_x, upper_left_y)
-
-	elif order > 0:		# 2-2. Recursive Case
-		# 1. Draw triangle on the window
-		width = length * 0.5
-		height = length * 0.866
-
-		line_top = GLine(upper_left_x, upper_left_y, upper_left_x+length, upper_left_y)
-		line_left = GLine(upper_left_x, upper_left_y, upper_left_x + width, upper_left_y + height)
-		line_right = GLine(upper_left_x + length, upper_left_y, upper_left_x + length - width, upper_left_y + height)
-
-		window.add(line_top)
-		window.add(line_left)
-		window.add(line_right)
-
-		pause(DELAY)
-
-		# 3. Recursion
+	if order == 0:
+		pass
+	else:
+		line1 = GLine(upper_left_x, upper_left_y, upper_left_x + length, upper_left_y)
+		line2 = GLine(upper_left_x, upper_left_y, upper_left_x + length / 2, upper_left_y + length*0.866)
+		line3 = GLine(upper_left_x + length, upper_left_y, upper_left_x + length/2, upper_left_y + length*0.866)
+		window.add(line1)
+		window.add(line2)
+		window.add(line3)
 		sierpinski_triangle(order-1, length/2, upper_left_x, upper_left_y)
-		sierpinski_triangle(order-1, length/2, upper_left_x+width, upper_left_y)
-		sierpinski_triangle(order-1, length/2, upper_left_x+(width/2), upper_left_y + (height/2))
+		sierpinski_triangle(order-1, length/2, upper_left_x + length/2, upper_left_y)
+		sierpinski_triangle(order-1, length/2, upper_left_x + length/4, upper_left_y + length*0.866 / 2)
 
 
 if __name__ == '__main__':
